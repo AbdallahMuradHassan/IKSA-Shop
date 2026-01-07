1827a66ca39ddd569ccaa9c95accd73b4b554443
AbdallahMuradHassan <abdallaha77770@gmail.com>
2026-01-07 20:37:53 +0300

    update

--- 7-1-26/in Class Task/requirements/js/object .js
+++ 7-1-26/in Class Task/requirements/js/object .js
@@ -1,23 +1,23 @@
 console.log("ssss");
 var x = 0;
 let prodect = [
-    { name: "char", dicrption: "thidsafasdfs sdfasd asdf asdf adsf asdf asdf ", price: "5 jd" },
-    { name: "door stap", dicrption: "thidsafasdfs sdfasd asdf ssdasdfsvcasdfvg sdfg  ", price: "5 jd" },
-    { name: "ceramic table", dicrption: "thidsafasdfs sdfasd asdf asdf adsf asdf asdf ", price: "5 jd" },
+    { name: "char", dicrption: "thidsafasdfs sdfasd asdf asdf adsf asdf asdf ", price: 5 },
+    { name: "door stap", dicrption: "thidsafasdfs sdfasd asdf ssdasdfsvcasdfvg sdfg  ", price: 10 },
+    { name: "ceramic table", dicrption: "thidsafasdfs sdfasd asdf asdf adsf asdf asdf ", price: 20 },
 
 ];
 
 
-const html = prodect.map(prodect => {
+const html = prodect.filter(price => price.price < 10).map(filter => {
     return `
         <div class="cards col-lg-4 col-md-6">
             <img src="../requirements/imges/prdects${++x}.jpg"
                  class="bd-placeholder-img rounded-circle"
                  width="140" height="140" />
 
-            <h2>${prodect.name}</h2>
-            <p>discrption: ${prodect.dicrption}</p>
-            <p>Phone: ${prodect.price}</p>
+            <h2>${filter.name}</h2>
+            <p>discrption: ${filter.dicrption}</p>
+            <p>Phone: ${filter.price}</p>
 
             <p>
                 <a class="btn btn-secondary login-register" href="#">
@@ -29,6 +29,6 @@ const html = prodect.map(prodect => {
 }).join("");
 document.getElementById("box").innerHTML = html;
 //accounts.forEach(account => {
-console.log(`account: name ${account.name}, email ${account.email}, phone ${account.phonenumper}`);
+//console.log(`account: name ${account.name}, email ${account.email}, phone ${account.phonenumper}`);
 
 //});
