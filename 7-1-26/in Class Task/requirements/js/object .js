8438b1c090e7a96fcf5c817cff2e12891071be3a
AbdallahMuradHassan <abdallaha77770@gmail.com>
2026-01-07 20:27:07 +0300

    update obgectjs

--- 7-1-26/in Class Task/requirements/js/object .js
+++ 7-1-26/in Class Task/requirements/js/object .js
@@ -1,16 +1,34 @@
 console.log("ssss");
-let accounts = [
-    { name: "ss", email: "ss@gmail.com", phonenumper: "+962788888888" },
-    { name: "yy", email: "yy@gmail.com", phonenumper: "+962788888888" },
-    { name: "rr", email: "rr@gmail.com", phonenumper: "+962788888888" },
-    { name: "tt", email: "tt@gmail.com", phonenumper: "+962788888888" }
+var x = 0;
+let prodect = [
+    { name: "char", dicrption: "thidsafasdfs sdfasd asdf asdf adsf asdf asdf ", price: "5 jd" },
+    { name: "door stap", dicrption: "thidsafasdfs sdfasd asdf ssdasdfsvcasdfvg sdfg  ", price: "5 jd" },
+    { name: "ceramic table", dicrption: "thidsafasdfs sdfasd asdf asdf adsf asdf asdf ", price: "5 jd" },
+
 ];
 
 
-accounts.map(account => {
-    console.log(`account: name ${account.name}, email ${account.email}, phone ${account.phonenumper}`);
-})
-accounts.forEach(account => {
-    console.log(`account: name ${account.name}, email ${account.email}, phone ${account.phonenumper}`);
+const html = prodect.map(prodect => {
+    return `
+        <div class="cards col-lg-4 col-md-6">
+            <img src="../requirements/imges/prdects${++x}.jpg"
+                 class="bd-placeholder-img rounded-circle"
+                 width="140" height="140" />
 
-});
+            <h2>${prodect.name}</h2>
+            <p>discrption: ${prodect.dicrption}</p>
+            <p>Phone: ${prodect.price}</p>
+
+            <p>
+                <a class="btn btn-secondary login-register" href="#">
+                    View details &raquo;
+                </a>
+            </p>
+        </div>
+    `; x++;
+}).join("");
+document.getElementById("box").innerHTML = html;
+//accounts.forEach(account => {
+console.log(`account: name ${account.name}, email ${account.email}, phone ${account.phonenumper}`);
+
+//});
